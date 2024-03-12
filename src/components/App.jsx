0ada3328ddcb49bf915e1d633d2ac14f02ef2746
Bob Hodges <bobhodges1@outlook.com>
2024-03-12 20:36:12 +0000

    images removed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import { CardGame } from "./CardGame";
+import { Footer } from "./Footer";
 
 export default function App() {
   return (
     <div>
       <h1> Welcome to Play Your Cards Right! </h1>
       <CardGame />
+      <div>
+        <hr />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,19 @@
 export function Footer() {
     return (
-        <footer>
-            Uses <a href="https://oxal.org/projects/sakura/">Sakura CSS.</a>
-            <br />
-            Uses{" "}
-            <a href="https://github.com/nbogie/demo-first-react-js/">
-                my source code.
-            </a>
-        </footer>
+      <footer>
+        Uses <a href="https://oxal.org/projects/sakura/">Sakura CSS.</a>
+        <br />
+        Uses{" "}
+        <a href="https://github.com/nbogie/demo-first-react-js/">
+          as source code.
+        </a>
+        <hr />
+        <a>
+          {" "}
+          Vectorized Playing Cards 1.3-
+          http://code.google.com/p/vectorized-playing-cards/ Copyright 2011 -
+          Chris Aguilar Licensed under LGPL 3 - www.gnu.org/copyleft/lesser.html
+        </a>
+      </footer>
     );
 }
